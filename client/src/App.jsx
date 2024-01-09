bab08577abaf5b2b4c4fba5f90e365e83e9aca72
shreyaan <shreyaan.saha.1009@gmail.com>
2024-01-09 21:29:05 +0530

    Completed Show User Listings functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './pages/createListing';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -16,7 +16,7 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-         <Route path='/about' element={<About />} />
+        <Route path='/about' element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
