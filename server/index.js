6675267cbbfe6129d53eefaf725556babe7f49a0
shreyaan <shreyaan.saha.1009@gmail.com>
2024-05-29 15:50:45 +0530

    Made minor changes

--- server/index.js
+++ server/index.js
@@ -21,9 +21,11 @@ mongoose
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 3000!');
 });
 app.use('/api/user', userRouter);
